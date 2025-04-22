b5c4868afb5e821cf35578fc6fffa8675b53d650
samithcsachi <samith.sachi@gmail.com>
2025-04-22 12:03:13 +0400

    folder stucture added

---
+++ template.py
@@ -0,0 +1,46 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+project_name = "chicken-Disease-Classification"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",  
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py", 
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entry/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trial.ipynb",
+   
+]
+    
+
+
+for filepath in list_of_files:
+    # create file path
+    file_path = Path(filepath)
+    filedir,filename = os.path.split(file_path)
+    # check if the directory exists
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file :{filename}")  
+    if (not os.path.exists(file_path)) or (os.path.getsize(file_path) == 0):
+        with open(file_path, 'w') as f:
+            pass
+            logging.info(f"Creating emplty file: {file_path}")
+    else:
+        logging.info(f"File already exists: {file_path}")
+
+       
