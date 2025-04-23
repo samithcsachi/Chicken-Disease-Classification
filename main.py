8833036c6d5896a889e75b0a838a3bf4224e8c24
samithcsachi <samith.sachi@gmail.com>
2025-04-23 12:27:52 +0400

    dvc added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from CNNClassification import logger
 from CNNClassification.pipeline.stage_01_data_ingestion import DataIngestionPipeline
 from CNNClassification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from CNNClassification.pipeline.stage_03_training import ModelTrainingPipeline
+from CNNClassification.pipeline.stage_04_evaluation import EvaluationPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -35,6 +36,22 @@ try:
     model_trainer = ModelTrainingPipeline()
     model_trainer.main()
     logger.info(f">>>>>>>>>>>>>>>>>> {STAGE_NAME} completed <<<<<<<<<<<<<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+
+
+STAGE_NAME = "Evaluation stage"
+
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = EvaluationPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
     raise e
--- src/CNNClassification/config/configuration.py
+++ src/CNNClassification/config/configuration.py
@@ -4,7 +4,7 @@ import os
 from pathlib import Path
 from  CNNClassification.utils.common import read_yaml, create_directories
 from CNNClassification.entity.config_entity  import (DataIngestionConfig,
-                                                PrepareBaseModelConfig,PrepareCallbacksConfig,TrainingConfig)
+                                                PrepareBaseModelConfig,PrepareCallbacksConfig,TrainingConfig,EvaluationConfig)
 
 
 
@@ -111,3 +111,14 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model=Path("artifacts/training/model.h5"),
+            training_data=Path("artifacts/data_ingestion/Chicken-fecal-images"),
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
